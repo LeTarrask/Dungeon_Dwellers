97266c354319d7be082c5766a5fa68f45f32d66a
Alex Luna <contato@tarrask.com>
2024-01-05 18:23:42 +0000

    Initial Commit

---
+++ Dungeon Dwellers/Dungeon_DwellersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dungeon_DwellersApp.swift
+//  Dungeon Dwellers
+//
+//  Created by Alex Luna on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dungeon_DwellersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
